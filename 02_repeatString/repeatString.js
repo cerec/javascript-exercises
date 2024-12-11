4b647c095063121befbe85b51aaeb9589691fc2c
Cerre <dcerrejon5@gmail.com>
2024-12-11 17:56:41 +0100

    exercise 2 done (all tests passed)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    if (num < 0) { return 'ERROR' }
+    let counter = 0;
+    let result = '';
+    while (counter < num) {
+        result = result.concat(string);
 
+        counter++;
+    }
+    return result;
 };
 
 // Do not edit below this line
